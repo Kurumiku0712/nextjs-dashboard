0e2e30610a666971a9df75a6af8ca31de2fcad06
Kurumiku0712 <kurumiku0712@gmail.com>
2025-02-20 22:37:50 +1100

    feat: Create Navbar

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,5 +1,16 @@
+import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/Sidebar";
+
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
-  return <div>DashboardWrapper {children}</div>;
+  return (
+    <div className="light flex w-full min-h-screen bg-gray-50 text-gray-900">
+      <Sidebar />
+      <main className="flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24">
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
 };
 
 export default DashboardWrapper;
